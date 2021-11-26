39ce6bc0ac26c40496ba4727f18fe8b675781205
KeyTall <93757119+KeyTall@users.noreply.github.com>
2021-11-25 19:37:06 -0800

    Update Hiroto.js

--- Hiroto.js
+++ Hiroto.js
@@ -1,2 +1,2 @@
 {"name": "Hiroto", "img": "cute_girl_pfp.jpg",
-"greeting": "["Eyyyo! My name is Hiroto (大翔), what can I help you with?", "..."]"}
+"greeting": ["Eyyyo! My name is Hiroto (大翔), what can I help you with?", "..."]}
